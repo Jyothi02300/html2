968c663fadb59364a8cb87523713045a1bc0b114
Jyothi02300 <mudavathj160@gmail.com>
2025-08-05 19:39:01 +0530

    Add files via upload

---
+++ javavar.js
@@ -0,0 +1,70 @@
+let month="march"
+switch(month){
+    case "january":
+        console.log("its a new starting ")
+        break;
+    case "febraury":
+        console.log("februry month with days")
+        break;
+    case "march":
+        console.log("its march with holi")
+        break;
+    case "april":
+        console.log("its april with summer")
+        break;
+    case "may":
+        console.log("its may with sunny day")
+        break;
+    case "june":
+        console.log("its june for farmers")
+        break;
+    case "july":
+        console.log("its july with rainy days")
+        break;
+    case "august":
+        console.log("its august with different festival")
+        break;
+    case "september":
+        console.log("its september")
+        break;
+    case "october":
+        console.log("its october with dussehra festival")
+        break;
+    case "november":
+        console.log("its november")
+        break;
+    case "december":
+        console.log("end of the year")
+        break;
+    default:
+        console.log("its not the month above case")    
+}
+
+let week="5"
+switch(week){
+    case "1":
+        console.log("sunday")
+         break;
+    case"2":
+        console.log("monday")
+         break;
+    case "3":
+        console.log("tuesday")
+      break;
+    case "4":
+        console.log("wednesday")
+         break;
+    case "5":
+         console.log("thursday")
+         break;
+    case "6":
+        console.log("friday")
+         break;
+    case "7":
+        console.log("saturday")
+        break;
+    default:
+        console.log("its not from the weeks")
+         break;
+
+}
