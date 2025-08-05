df2c23d7fc98dc90aefa807b09c4675db1717967
Jyothi02300 <mudavathj160@gmail.com>
2025-08-05 19:35:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+// //primitive datatypes
+// var full_name="komali";//string
+// console.log(typeof full_name);
+// var age=65;//number
+// console.log(typeof age);
+// var isfollow=false;//boolean
+// console.log(typeof isfollow);
+// var branch;//undefined
+// console.log(typeof branch);
+// var phone_number=null;//null
+// console.log(typeof phone_number);
+// var bignumber=2354n;//bigit
+// console.log(typeof bignumber);
+// var id=symbol('id');//symbol
+// console.log(typeof id);
+
+var employee={
+    emp_id:4, 
+    employee_name:"ramesh",
+    Role:"SDE",
+    isemp:true
+};
+console.log(employee["emp_id"])
+console.log(employee["employee_name"])
+console.log(employee["Role"])
+console.log(employee["isemp"])
