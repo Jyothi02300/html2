38f870dfeee6c914aad7ae9ff9139f9413b77ba7
Jyothi02300 <mudavathj160@gmail.com>
2025-08-05 19:39:25 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,45 @@
+
+
+let name="welcome";
+for(let fullname of name)
+    {
+    console.log(fullname + 1234);
+}
+
+let arr=[25,26,35,38,50,60]
+for(i=0;i<=arr.length;i++)
+    console.log(arr[i])
+
+
+let student = {
+    key: "Value",     
+    name: "Jyothi",
+    Rollno: 40,
+    branch: "IT"
+};
+
+for (let details in student) {
+    console.log(details);
+    console.log(student[details]);
+}
+
+
+ for(let i=2;i<=100;i+=2)
+ {
+    console.log(i)
+ }
+
+ let numbers=[5,6,7,8,9];
+ let squares=[];
+ for(let num of numbers){
+    squares.push(num*num);
+ }
+ console.log("squares");
+
+ let str1="welcome to mrec college")
+ let str2="campus"
+
+ //strings concepts
+ //methods 
+
+
