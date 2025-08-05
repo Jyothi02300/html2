b1c1ea37adee3893d01f574bbcaec68556c3018d
Jyothi02300 <mudavathj160@gmail.com>
2025-08-05 19:36:05 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,50 @@
+//2.assignment operator
+let a=36
+let b=20
+console.log("a +=b:", a +=b);//a =a+b
+console.log("a -=b:", a -=b);//a =a-b
+console.log("a *=b:", a *=b);//a =a*b
+console.log("a %=b:", a %=b);//a =a%b
+console.log("a *=b:", a **=b);//a =a*b
+
+// //comparison operator
+// let c=8
+// let d=8
+// console.log("c >d:", c>d);//8>3
+// console.log("c >=d:", c >=d);//8>=3
+// console.log("c <d:", c <d);//8<4
+// console.log("c <=d:", c <=d);//8=<4
+// console.log("c !=d:", c !=d);//8!=8
+// console.log("c===d:",c===d)//8===8
+
+// let rating=2;
+
+// if(rating >= 8 || rating <=5){
+//     console.log("the movie is blockbuster");
+// }
+// else if(rating >= 4 || rating >=3){
+//     console.log("the movie is super hit");
+// }
+// else if(rating <=4 && rating >=3){
+//     console.log("the movie hit of the day")                     
+// }
+// else if(rating <=2 && rating >= 1){
+//     console.log("the movie is just average")
+// }
+// else{
+//     console.log("the movie is flop")
+// }
+
+
+let age =21
+let result=age>=18?"adult":"minor";
+console.log(result)
+
+let marks=30
+//25>=30
+let results= marks>=30?"pass marks":"fail";
+console.log(results)
+
+
+//2 .
+let 
