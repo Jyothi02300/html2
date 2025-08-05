bdf49408e58aaacf22472e99d12a765c2979c17b
Jyothi02300 <mudavathj160@gmail.com>
2025-08-05 19:42:45 +0530

    Add files via upload

---
+++ ternary operator.js
@@ -0,0 +1,14 @@
+let score = 60;
+let grade = (score>=90)?"A":
+            (score>=80)?"B":
+            (score>=70)?"C":"F";
+        console.log(grade);
+        console.log(score);
+//
+prompt
+let age = 20
+let name=prompt("enter your name");
+let message=(age>=18)?"YOU ARE AN ADULT":"YOU ARE ANA"
+console.log(message);
+console.log(age);
+//
