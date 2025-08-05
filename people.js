66709b057fe9b00a138ecc40848f4fff32fb9b61
Jyothi02300 <mudavathj160@gmail.com>
2025-08-05 19:41:19 +0530

    Add files via upload

---
+++ people.js
@@ -0,0 +1,23 @@
+const people = [
+  {
+    name: "Spongebob",
+    age: 30,
+    isEmployed: true
+  },
+  {
+    name: "Patrick",
+    age: 34,
+    isEmployed: false
+  },
+  {
+    name: "Squidward",
+    age: 50,
+    isEmployed: true
+  },
+  {
+    name: "Cummins",
+    age: 27,
+    isEmployed: false
+  }
+];
+
