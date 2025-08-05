d04b01649cf8417ef4686356d2882597db0d62dc
Jyothi02300 <mudavathj160@gmail.com>
2025-08-05 19:36:49 +0530

    Add files via upload

---
+++ dialogbox.js
@@ -0,0 +1,10 @@
+alert("welcome!");
+var female = confirm("Are you a female") 
+if(female){
+    let name = prompt("Enter your name:");
+    document.writeln("Hello madam"+"welcome to my page")
+}
+else{
+    let name = prompt("Enter your name");
+    document.writeln("Hello Mr"+"welcome to my page");
+}
